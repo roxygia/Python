95b6f141349d9f6977aa57d436eb626fab876403
roxygia <62426542+roxygia@users.noreply.github.com>
2020-06-28 09:39:46 +0800

    Added solution to Ex2 Q3
     Write a program that implements the algorithm for Red Light Cameras.

---
+++ exercises/booleans_q3.py
@@ -0,0 +1,50 @@
+#Q3) Write a program that implements the algorithm for Red Light Cameras.
+
+light_colour = "Red"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+
+light_colour = "Red"
+car_detected = True
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Green"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Green"
+car_detected = True
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Amber"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
+
+light_colour = "Amber"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do nothing.")
