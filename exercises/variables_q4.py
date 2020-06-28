855eeb976b7b95cd5d55a8abc6a87e871c8fa557
roxygia <62426542+roxygia@users.noreply.github.com>
2020-06-28 09:07:53 +0800

    Added solution to ex1 Q4

---
+++ exercises/variables_q4.py
@@ -0,0 +1,9 @@
+#Q4) Write a program that takes the users name and height 
+# (in centimeters), and outputs a summary sentence
+
+print("This program will take user's name andd height in cm to output summary sentence")
+name = input("Enter name: ")
+height = int(input("Enter height in cm: "))
+
+#print name and age in one sentence
+print(f"{name} is {height}cms tall.")
