563733de4e9bfb747582cd2f2f79409b3c6a2b4c
roxygia <62426542+roxygia@users.noreply.github.com>
2020-07-05 21:34:36 +0800

    completed functions exercises
    added q3, q4

--- reading_writing_files/exercise_data/functions_q3.py
+++ reading_writing_files/exercise_data/functions_q3.py
@@ -1,8 +1,25 @@
+import csv
+
 def read_csv_file(filepath):
     import csv
-    with open("colours_20.csv") as csv_file:
-    reader = csv.reader(csv_file, delimiter=",")
+    with open(filepath) as csv_file:
+        #make sure you cast the reader into a "list" before returning it if you open csv
+        #without casting you returne a generator on the file object - not fully processed.
+        # That file was closed when you left the with block in the function.
+        reader = list(csv.reader(csv_file, delimiter=","))
+    return reader
 
 
 def format_colour_line(colour_data):
-    return
+    for line in colour_data:
+      print(f"{line[1]:<15} {line[2]:<10} {line[4]:<20}")
+
+
+colours = read_csv_file("colours_20.csv")
+format_colour_line(colours)
+
+colours = read_csv_file("colours_213.csv")
+format_colour_line(colours)
+
+
+
